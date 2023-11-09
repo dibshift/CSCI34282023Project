3efa5dce60c8f48ca268b922c92eec7eb2ed05ce
IceMemeSandwich <robarflynn@gmail.com>
2023-11-09 16:10:41 -0400

    Added '@vitejs/plugin-basic-ssl' for local testing

--- mobilemap/vite.config.js
+++ mobilemap/vite.config.js
@@ -1,8 +1,16 @@
+import basicSsl from '@vitejs/plugin-basic-ssl'
+// ! WARNING
+// ! Most of this will need to be changed before release as the setting are 
+// ! made for local testing
 export default {
   build: {
     sourcemap: true,
   },
+  // TODO: Change for release?
   server: {
     host: true
-  }
+  },
+  // TODO: 100% Change for release
+  // Needed so geolocation works for mobile
+  plugins: [basicSsl()]
 }
