18feb0cde5e36285d4a2310702b2753a7556db00
IceMemeSandwich <robarflynn@gmail.com>
2023-11-15 15:29:14 -0400

    More Copied Code for main.js

--- mobilemap/main.js
+++ mobilemap/main.js
@@ -4,6 +4,7 @@ import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import {fromLonLat} from 'ol/proj.js';
 
+import LineString from 'ol/geom/LineString.js';
 import Feature from 'ol/Feature.js';
 import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style.js';
 import {Vector as VectorSource} from 'ol/source.js';
@@ -34,7 +35,9 @@ const map = new Map({
 // User location variable
 const geolocation = new Geolocation({
   trackingOptions: {
+    maximumAge: 10000,
     enableHighAccuracy: true,
+    timeout: 600000,
   },
   // I don't know what this is but all the examples have it so it might be a good idea
   projection: view.getProjection(),
@@ -44,11 +47,19 @@ allowTrackingbtn.addEventListener(
   'click',
   function () {
     geolocation.setTracking(true);
+    tileLayer.on('postrender', updateView);
+    map.render();
     bsOffcanvas.hide();
   }
 )
 
 // Stollen code for testing
+
+// LineString to store the different geolocation positions. This LineString
+// is time aware.
+// The Z dimension is actually used to store the rotation (heading).
+const positions = new LineString([], 'XYZM');
+let deltaMean = 500;
 
 // handle geolocation error.
 // ! TODO: figure out the best way to show the error
@@ -80,7 +91,52 @@ positionFeature.setStyle(
 geolocation.on('change:position', function () {
   const coordinates = geolocation.getPosition();
   positionFeature.setGeometry(coordinates ? new Point(coordinates) : null);
+  
+  const position = geolocation.getPosition();
+  const accuracy = geolocation.getAccuracy();
+  const heading = geolocation.getHeading() || 0;
+  const speed = geolocation.getSpeed() || 0;
+  const m = Date.now();
+
+  addPosition(position, heading, m, speed);
+
+  const coords = positions.getCoordinates();
+  const len = coords.length;
+  if (len >= 2) {
+    deltaMean = (coords[len - 1][3] - coords[0][3]) / (len - 1);
+  }
+  alert(coords)
 });
+
+let previousM = 0;
+function addPosition(position, heading, m, speed) {
+  const x = position[0];
+  const y = position[1];
+  const fCoords = positions.getCoordinates();
+  const previous = fCoords[fCoords.length - 1];
+  const prevHeading = previous && previous[2];
+  if (prevHeading) {
+    let headingDiff = heading - mod(prevHeading);
+
+    // force the rotation change to be less than 180°
+    if (Math.abs(headingDiff) > Math.PI) {
+      const sign = headingDiff >= 0 ? 1 : -1;
+      headingDiff = -sign * (2 * Math.PI - Math.abs(headingDiff));
+    }
+    heading = prevHeading + headingDiff;
+  }
+  positions.appendCoordinate([x, y, heading, m]);
+
+  // only keep the 20 last coordinates
+  positions.setCoordinates(positions.getCoordinates().slice(-20));
+
+  // FIXME use speed instead
+  // if (heading && speed) {
+  //   markerEl.src = 'data/geolocation_marker_heading.png';
+  // } else {
+  //   markerEl.src = 'data/geolocation_marker.png';
+  // }
+}
 
 new VectorLayer({
   map: map,
@@ -89,6 +145,30 @@ new VectorLayer({
   }),
 });
 
+function updateView() {
+  // use sampling period to get a smooth transition
+  let m = Date.now() - deltaMean * 1.5;
+  m = Math.max(m, previousM);
+  previousM = m;
+  // interpolate position along positions LineString
+  const c = positions.getCoordinateAtM(m, true);
+  if (c) {
+    view.setCenter(getCenterWithHeading(c, -c[2], view.getResolution()));
+    view.setRotation(-c[2]);
+    // marker.setPosition(c);
+    map.render();
+  }
+}
+
+function getCenterWithHeading(position, rotation, resolution) {
+  const size = map.getSize();
+  const height = size[1];
+
+  return [
+    position[0] - (Math.sin(rotation) * height * resolution * 1) / 4,
+    position[1] + (Math.cos(rotation) * height * resolution * 1) / 4,
+  ];
+}
 
 // End of stollen code
 function setup() {
