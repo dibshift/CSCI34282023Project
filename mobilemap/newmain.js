a1b9e9356527a3ff7ea17cd1efb57411294bf510
IceMemeSandwich <robarflynn@gmail.com>
2023-11-24 16:22:29 -0400

    slimed down and replaced main.js  (mobilemap)

--- mobilemap/newmain.js
+++ mobilemap/newmain.js
@@ -4,10 +4,6 @@ import TileLayer from 'ol/layer/Tile';
 // OpenSourceMap
 import OSM from 'ol/source/OSM';
 import {fromLonLat} from 'ol/proj.js';
-
-// Array for holding positions that have been gained from the gps?
-import LineString from 'ol/geom/LineString.js';
-// 
 import Feature from 'ol/Feature.js';
 // Used for creating the location dot
 import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style.js';
@@ -57,7 +53,7 @@ allowTrackingbtn.addEventListener(
   'click',
   function () {
     geolocation.setTracking(true);
-    tileLayer.on('postrender', updateView);
+    // tileLayer.on('postrender', updateView);
     map.render();
     bsStartOffcanvas.hide();
   }
@@ -68,8 +64,6 @@ allowTrackingbtn.addEventListener(
 // LineString to store the different geolocation positions. This LineString
 // is time aware.
 // The Z dimension is actually used to store the rotation (heading).
-const positions = new LineString([], 'XYZM');
-let deltaMean = 500;
 
 // handle geolocation error.
 // ! TODO: figure out the best way to show the error
@@ -96,22 +90,27 @@ positionFeature.setStyle(
 );
 
 geolocation.on('change', function () {
-  
-  const position = geolocation.getPosition();
-  const accuracy = geolocation.getAccuracy();
-  const heading = geolocation.getHeading() || 0;
-  const speed = geolocation.getSpeed() || 0;
-  const m = Date.now();
+    positionFeature.setGeometry(geolocation.getPosition() ? new Point(geolocation.getPosition()) : null);
+    accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+    // map.render();
 
-  addPosition(position, heading, m, speed);
+  // Center the view on the positionFeature
+  view.setCenter(positionFeature.getGeometry().getCoordinates());
 
-  const coords = positions.getCoordinates();
-  const len = coords.length;
-  if (len >= 2) {
-    deltaMean = (coords[len - 1][3] - coords[0][3]) / (len - 1);
-  }
-
-  updateView();
+  // Clean 
+// takes information from a placed geometry. (from GeoJSON) and makes a list of info from that map.
+// TODO: Make it so that it only does that when entering an area
+vectorLayerz.getFeatures(map.getPixelFromCoordinate(geolocation.getPosition())).then(function (features) {
+  const feature = features.length ? features[0] : undefined;
+    if (features.length) {;
+      if (feature.get("NAME") != tempVar && tempVar != undefined && feature.get("NAME") != undefined) {
+        tempVar = feature.get('NAME');
+        displayInformation(feature)
+      }
+    } else {
+      //alert('No features found');
+    }
+  });
 });
 
 const vectorLayerz = new VectorLayer({
@@ -124,76 +123,6 @@ const vectorLayerz = new VectorLayer({
   }),
 });
 
-
-function addPosition(position, heading, m, speed) {
-  const x = position[0];
-  const y = position[1];
-  const fCoords = positions.getCoordinates();
-  const previous = fCoords[fCoords.length - 1];
-  const prevHeading = previous && previous[2];
-  if (prevHeading) {
-    let headingDiff = heading - mod(prevHeading);
-
-    // force the rotation change to be less than 180°
-    if (Math.abs(headingDiff) > Math.PI) {
-      const sign = headingDiff >= 0 ? 1 : -1;
-      headingDiff = -sign * (2 * Math.PI - Math.abs(headingDiff));
-    }
-    heading = prevHeading + headingDiff;
-  }
-  positions.appendCoordinate([x, y, heading, m]);
-
-  // only keep the 20 last coordinates
-  positions.setCoordinates(positions.getCoordinates().slice(-20));
-
-}
-
-let previousM = 0;
-// ! this (or add position) should be the base for things to happen and NOT change position
-function updateView() {
-  // use sampling period to get a smooth transition
-  let m = Date.now() - deltaMean * 1.5;
-  m = Math.max(m, previousM);
-  previousM = m;
-  // interpolate position along positions LineString
-  const c = positions.getCoordinateAtM(m, true);
-  if (c) {
-    view.setCenter(getCenterWithHeading(c, -c[2], view.getResolution()));
-    // view.setRotation(-c[2]);
-    positionFeature.setGeometry(geolocation.getPosition() ? new Point(geolocation.getPosition()) : null);
-    accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
-    // map.render();
-
-    map.getView().setRotation(-c[2]);
-    // Center the view on the positionFeature
-    view.setCenter(positionFeature.getGeometry().getCoordinates());
-
-    // Clean 
- // takes information from a placed geometry. (from GeoJSON) and makes a list of info from that map.
-  // TODO: Make it so that it only does that when entering an area
-  vectorLayerz.getFeatures(map.getPixelFromCoordinate(geolocation.getPosition())).then(function (features) {
-    const feature = features.length ? features[0] : undefined;
-      if (features.length) {;
-        if (feature.get("NAME") != tempVar && tempVar != undefined && feature.get("NAME") != undefined) {
-          tempVar = feature.get('NAME');
-          displayInformation(feature)
-        }
-      } else {
-        //alert('No features found');
-      }
-    });
-  }
-}
-
-function getCenterWithHeading(position, rotation, resolution) {
-  const size = map.getSize();
-  const height = size[1];
-
-  return [
-    position[0] - (Math.sin(rotation) * height * resolution * 1) / 4,
-    position[1] + (Math.cos(rotation) * height * resolution * 1) / 4,
-  ];
-}
 
 // This is a backup, the user can also tap 
 map.on('click', function (evt) {
