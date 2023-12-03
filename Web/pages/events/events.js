0511d240bee23c0523cfc7a4f041efc84ba1dcad
alexcarr42 <146783756+alexcarr42@users.noreply.github.com>
2023-12-03 18:22:01 -0400

    Update events.js

--- Web/pages/events/events.js
+++ Web/pages/events/events.js
@@ -1,3 +1,8 @@
+/*Authors: Alex Carreiro, ChatGPT
+  This file provides functionality for events.html
+  It gets events posted to the server as name value pairs
+  and displays them on a carousel
+*/
 
 // Execute the following code when the DOM content has been fully loaded
 document.addEventListener('DOMContentLoaded', function () {
@@ -17,17 +22,22 @@ document.addEventListener('DOMContentLoaded', function () {
     .catch(error => console.error('Error fetching events:', error));
 });
 
+/*This function populates the carousel in events.html with event data posted on the admin page
+  It takes events as an argument, which is the event data from the server
+*/ 
 function populateCarousel(events) {
   const carouselInner = document.querySelector('#eventsCarousel .carousel-inner');
   // Take only the next three events
   const upcomingEvents = events.slice(0, 3);
 
+  // Loop through the upcoming events to create carousel items
   upcomingEvents.forEach(function (event, index) {
     const eventItem = document.createElement('div');
     eventItem.classList.add('carousel-item');
     if (index === 0) {
       eventItem.classList.add('active');
     }
+    //event details to be added
     eventItem.innerHTML = `
       <div class='indivEvent'>
         <div class='eventDetails'>
